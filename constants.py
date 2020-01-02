b11646ebf1c0284d12a75d766a1ffc30b89e5d82
Cirill Usatchoff <usatchoff@gmail.com>
2020-01-02 21:00:17 +0200

    SmartWrapper implemented

---
+++ constants.py
@@ -0,0 +1,2 @@
+KEYWORD_VALUE_ERROR: str = 'Wrapper takes no keyword arguments.'
+NOT_SINGLE_VALUE_ERROR: str = 'You can put only one value to wrapper.'
---
+++ smart_wrapper.py
@@ -0,0 +1,22 @@
+from typing import Any
+
+
+class SmartWrapper:
+    def __init__(self, value: Any) -> None:
+        self.__value = value
+
+    def __call__(self, value: Any = None) -> Any:
+        if value:
+            self.__value = value
+        else:
+            return self.__value
+
+    def gettype(self) -> type:
+        return type(self.__value)
+
+    def hasattr(self, attr: str) -> bool:
+        return hasattr(self.__value, attr)
+
+
+def wrap(value: Any) -> SmartWrapper:
+    return SmartWrapper(value)
